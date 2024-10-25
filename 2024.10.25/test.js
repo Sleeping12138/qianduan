85a53b945f406c305b92a2760bf018446817245b
Sleeping <143491797+Sleeping12138@users.noreply.github.com>
2024-10-25 22:05:55 +0800

    Add files via upload

---
+++ 2024.10.25/test.js
@@ -0,0 +1,26 @@
+const express = require('express');
+let app = express();
+app.get('/test', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send('hello world');
+})
+app.listen(9000, () => {
+    console.log('9000端口已经启动...');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
