8aedd8f1b05aacf31eeece58922906375e6a5bc6
Sleeping <143491797+Sleeping12138@users.noreply.github.com>
2025-01-10 16:38:37 +0800

    Add files via upload

---
+++ 2025.1.10/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 ref="hh">hello world</h1>
+        <button @click="btn">点击获取数据</button>
+        <Student ref="student" name="zhangsan" :age="18"></Student>
+    </div>
+</template>
+
+<script>
+    import Student from './components/Student.vue'
+    export default {
+        name:'App',
+        components:{Student},
+        methods:{
+            btn(){
+                console.log(this.$refs.hh);
+                console.log(this.$refs.student);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 2025.1.10/components/Student.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="temp">
+        <h1>{{ msg }}</h1>
+        <div>{{ name }}</div>
+        <div>{{ newAge+1 }}</div>
+        <button @click="btn">点击我age++</button>
+    </div>
+</template>
+
+<script>
+    export default {    
+        name:'Student',
+        data() {
+            return {
+                msg:'hello world',
+                // 使用其他变量进行中转
+                newAge:this.age
+            }
+        },
+        // props:['name','age']
+        // props:{
+        //     name:String,
+        //     age:Number
+        // },
+
+        // 通过标签进行数据的传递
+        props:{
+            name:{
+                type:String,
+                required:true,
+            },
+            age:{
+                type:Number,
+                default:100
+            }
+        },
+        methods: {
+            btn(){
+                this.newAge++
+            }
+        },
+    }
+</script>
+
+<style>
+    .temp{
+        background-color: aquamarine;
+    }
+</style>
